0f85370acb650167d7d36e7f2a94742a4c8dd853
PROFESSSOR-BERLIN <125130880+PROFESSSOR-BERLIN@users.noreply.github.com>
2023-02-28 19:43:39 +0530

    Update insert.js
    For the latest version insertMany would work

--- insert.js
+++ insert.js
@@ -2,7 +2,7 @@ const dbConnect = require('./mongodb');
 
 const insertData=async ()=>{
    let data = await dbConnect();
-  let result = await data.insert(
+  let result = await data.insertMany(
       [
           {name:'max 5',brand:'micromax',price:420,category:'mobile'},
           {name:'max 6',brand:'micromax',price:520,category:'mobile'},
